8dc8f3a1d1dae17fcf73e3c9fc404da38a27b722
Ajay joseph <aj2506039@gmail.com>
2025-09-30 16:31:16 +0530

    feat:updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,20 +8,13 @@ import Signup from "./pages/Signup.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import CreateEvent from "./pages/CreateEvent.jsx";
 import ProtectedRoute from "./components/protectedRouter.jsx";
-import PublicEvent from "./components/PublicEvent.jsx";
+import PublicEvent from "./components/PublicEvent.jsx"
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <Route
-    path="/"
-    element={
-      <div className="flex items-center justify-center min-h-screen bg-green-100">
-        <h1 className="text-4xl font-bold text-green-900">Welcome to Eventify</h1>
-      </div>
-    }
-  />
+      
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
