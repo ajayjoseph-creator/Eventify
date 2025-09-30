5a9c9d9f5bcc7313b9b34528b3d408960509c8a0
Ajay joseph <aj2506039@gmail.com>
2025-09-30 16:54:47 +0530

    feat:

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -2,20 +2,25 @@ import User from "../models/User.js";
 import generateToken from "../utils/generateToken.js";
 
 
+
+
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password, confirmPassword } = req.body;
-    console.log("REQ BODY:", req.body);  
 
+ 
     const userExists = await User.findOne({ email });
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const user = new User({ name, email, password });
-    user.confirmPassword = confirmPassword;
+    
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
 
-    await user.save(); 
+    const user = new User({ name, email, password });
+    await user.save();
 
     res.status(201).json({
       _id: user._id,
@@ -24,10 +29,11 @@ export const registerUser = async (req, res) => {
       token: generateToken(user._id),
     });
   } catch (error) {
-    console.error("REGISTER ERROR:", error); 
+    console.error("REGISTER ERROR:", error);
     res.status(500).json({ message: error.message });
   }
 };
+
 
 
 
